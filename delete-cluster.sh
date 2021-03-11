69654b993434a2eb5d617cfbd7ed4497d355137c
JJ Asghar <awesome@ibm.com>
2021-03-11 11:36:06 -0600

    pass along the API key
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME=${1:mycluster-free}
+NAME=${2:mycluster-free}
 
 # Verify that the ibmcloud cli is there
 which ibmcloud > /dev/null 2>&1 || (echo "Install ibmcloud first before running this scripts: https://cloud.ibm.com/docs/cli" && exit 1)
@@ -9,7 +9,7 @@ which ibmcloud > /dev/null 2>&1 || (echo "Install ibmcloud first before running 
 ibmcloud plugin install -f kubernetes-service
 
 # Log into the IBM Cloud
-ibmcloud login -r us-south
+ibmcloud login --apikey $1 -r us-south
 
 # Delete your free cluster
 ibmcloud ks cluster rm --cluster ${NAME} --force-delete-storage -f
