fe480d6ee0ac0aed2da867a69b05332eb6fa1602
JJ Asghar <awesome@ibm.com>
2021-03-11 11:58:19 -0600

    removed env
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -5,8 +5,6 @@ NAME=${1:mycluster-free}
 # Verify that the ibmcloud cli is there
 which ibmcloud > /dev/null 2>&1 || (echo "Install ibmcloud first before running this scripts: https://cloud.ibm.com/docs/cli" && exit 1)
 
-env
-
 # Make sure that ks plugin is there
 ibmcloud plugin install -f kubernetes-service
 
